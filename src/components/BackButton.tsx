e25583a2e107b9b2c1dc29474b2b1ec0998d919a
Nation <nation.shakya@yipl.com.np>
2021-12-06 15:27:41 +0545

    Back Button Design

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 //dependencies
 import {useNavigation} from '@react-navigation/native';
-import {TouchableOpacity, Text} from 'react-native';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface Props {
   title: string;
@@ -12,11 +13,31 @@ const BackButton: React.FC<Props> = ({title}) => {
   const navigation = useNavigation();
   return (
     <>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>{title}</Text>
-      </TouchableOpacity>
+      <LinearGradient
+        colors={['#B471DA', '#D390AF']}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        style={styles.backBtn}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.textStyle}>{title}</Text>
+        </TouchableOpacity>
+      </LinearGradient>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  backBtn: {
+    alignSelf: 'flex-start',
+    paddingHorizontal: 30,
+    paddingVertical: 8,
+    borderBottomRightRadius: 50,
+    marginBottom: 16,
+  },
+  textStyle: {
+    textTransform: 'uppercase',
+    color: '#fff',
+  },
+});
 
 export default BackButton;
